a51cd4db5fd99c5f3bfbcdb77346ff1178033c9d
sophnagle <sophnagle@gmail.com>
2021-11-13 17:04:00 +0000

    remove scale js as not needed, its in css

--- assets/js/upScale.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    /* Enlarge effect */
-    $(".scale").hover(function() {
-        $(".scale").addClass('transition');
-
-    }, function() {
-        $(".scale").removeClass('transition');
-    });
-});
